4c6a265aaef2587e85f7dd5c9bd10ed1585cdd40
emi4218 <emi4218@gmail.com>
2021-10-06 10:58:03 +0200

    06/10/21 terminé et étonnamment pas trop moche

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,20 @@
 window.addEventListener('load', () => {
 
+    let modale = document.querySelector('.modale');
+    let contain = modale.querySelector('.contain')
+    let images = document.querySelectorAll('.image');
+    modale.querySelector('.close').addEventListener('click', event => {
+        modale.classList.remove('open');
+    })
+
+    images.forEach(image => {
+        image.addEventListener('click', event => {
+            console.log(image);
+            contain.innerHTML = "";
+            contain.appendChild(image.querySelector('img').cloneNode());
+            modale.classList.add('open');
+        })
+    });
 
 
 
